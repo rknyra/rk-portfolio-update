394e80e8e56ce33e45a99767778ccaa4018bebab
RKNyra <rk.nyra@gmail.com>
2020-01-03 10:34:53 +0300

    create the about-me page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,13 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, INJECTOR, Inject } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(NgbCarouselConfig) carousel: NgbCarouselConfig) {
+    carousel.interval = 3000;
+    carousel.keyboard = true;
+    carousel.wrap = true;
+    carousel.showNavigationArrows = true;
+  }
 
   ngOnInit() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
